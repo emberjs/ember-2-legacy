d6bc4d19ca4d54b16c428b3ff7745a7d3f827d09
Travis Hoover <thoover@linkedin.com>
2017-11-29 14:27:57 -0800

    Moving Ember.K to legacy addon

---
+++ tests/unit/ember-k-test.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+
+module('Ember.K');
+
+test('that Ember.K is correctly polyfilled', assert => {
+  assert.equal(typeof Ember.K, 'function', 'function is defined on Ember');
+});
+
+test('that Ember.K properly returns this when invoked', function(assert) {
+  let Thing = Ember.Object.extend({
+    randomMethod: Ember.K,
+  });
+
+  let instance = Thing.create();
+  assert.strictEqual(instance.randomMethod(), instance, 'Ember.K returns this');
+});
+
+test('that Ember.K is deprecated', function(assert) {
+  assert.expectDeprecation(() => {
+    let obj = {
+     noop: Ember.K
+    };
+
+    assert.equal(obj, obj.noop());
+  }, 'Ember.K is deprecated in favor of defining a function inline.');
+});
