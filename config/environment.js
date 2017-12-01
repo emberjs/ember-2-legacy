b05f3f9d69e6d3c15556a3e88c2c57e6fcce081c
gowtham.mk <gowtham.mk@zohocorp.com>
2017-12-02 03:09:35 +0530

    Add `_ENABLE_RESOVER_FUNCTION_SUPPORT` to config

--- config/environment.js
+++ config/environment.js
@@ -9,7 +9,8 @@ module.exports = function(/* environment, appConfig */) {
       _ENABLE_WARN_OPTIONS_SUPPORT: true,
       _ENABLE_SAFE_STRING_SUPPORT: true,
       _ENABLE_ENUMERABLE_CONTAINS_SUPPORT: true,
-      _ENABLE_UNDERSCORE_ACTIONS_SUPPORT: true
+      _ENABLE_UNDERSCORE_ACTIONS_SUPPORT: true,
+      _ENABLE_RESOLVER_FUNCTION_SUPPORT: true
     }
   };
 };
