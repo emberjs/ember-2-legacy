660cbac15b2bd99d33af10b71a80b510a754ee33
Travis Hoover <thoov7@gmail.com>
2017-12-12 10:58:21 -0800

    Add Ember.RouterDSL.resource support

---
+++ vendor/router-resource.js
@@ -0,0 +1,23 @@
+(function() {
+  var _Ember;
+  if (typeof Ember !== 'undefined') {
+    _Ember = Ember;
+  } else {
+    _Ember = require('ember').default;
+  }
+
+  if (EmberENV && EmberENV._ENABLE_ROUTER_RESOURCE !== true) {
+    return false;
+  }
+
+  _Ember.RouterDSL.prototype.resource = function(name, options = {}, callback) {
+    if (arguments.length === 2 && typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+
+    options.resetNamespace = true;
+    _Ember.deprecate('this.resource() is deprecated. Use this.route(\'name\', { resetNamespace: true }, function () {}) instead.', false, { id: 'ember-routing.router-resource', until: '3.0.0' });
+    this.route(name, options, callback);
+  }
+})();
