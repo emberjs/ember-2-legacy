79cbbe7f645095ec98fc8b02b1971dd14f1d57b1
Travis Hoover <thoov7@gmail.com>
2017-12-13 14:59:17 -0800

    Use ember.linkcomponent

--- vendor/current-when.js
+++ vendor/current-when.js
@@ -10,9 +10,7 @@
     return false;
   }
 
-  var linkTo = _Ember.__loader.require('ember-glimmer/components/link-to').default;
-
-  linkTo.reopen({
+  _Ember.LinkComponent.reopen({
     currentWhen: _Ember.computed.deprecatingAlias('current-when', {
       id: 'ember-routing-view.deprecated-current-when',
       until: '3.0.0'
