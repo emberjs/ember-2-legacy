c9d6fe62cc87f8055186a71cde19885163d2d434
Travis Hoover <thoov7@gmail.com>
2018-01-03 10:03:24 -0800

    Add references to deprecation docs for each flag

--- config/environment.js
+++ config/environment.js
@@ -4,26 +4,26 @@
 module.exports = function(/* environment, appConfig */) {
   return {
     EmberENV: {
-      _ENABLE_EMBER_K_SUPPORT: true,
-      _ENABLE_DEPRECATION_OPTIONS_SUPPORT: true,
-      _ENABLE_ORPHANED_OUTLETS_SUPPORT: true,
-      _ENABLE_WARN_OPTIONS_SUPPORT: true,
-      _ENABLE_SAFE_STRING_SUPPORT: true,
-      _ENABLE_ENUMERABLE_CONTAINS_SUPPORT: true,
-      _ENABLE_UNDERSCORE_ACTIONS_SUPPORT: true,
-      _ENABLE_RESOLVER_FUNCTION_SUPPORT: true,
-      _ENABLE_DID_INIT_ATTRS_SUPPORT: true,
-      _ENABLE_RENDER_SUPPORT: true,
-      _ENABLE_REVERSED_OBSERVER_SUPPORT: true,
-      _ENABLE_INITIALIZER_ARGUMENTS_SUPPORT: true,
-      _ENABLE_ROUTER_RESOURCE: true,
-      _ENABLE_CURRENT_WHEN_SUPPORT: true,
+      _ENABLE_EMBER_K_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_code-ember-k-code
+      _ENABLE_DEPRECATION_OPTIONS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_id-ember-debug-deprecate-test-as-function
+      _ENABLE_ORPHANED_OUTLETS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_rendering-into-a-render-helper-that-resolves-to-an-outlet
+      _ENABLE_WARN_OPTIONS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_id-ember-debug-deprecate-test-as-function
+      _ENABLE_SAFE_STRING_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_use-ember-string-htmlsafe-over-ember-handlebars-safestring
+      _ENABLE_ENUMERABLE_CONTAINS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_enumerable-contains
+      _ENABLE_UNDERSCORE_ACTIONS_SUPPORT: true, // N/A
+      _ENABLE_RESOLVER_FUNCTION_SUPPORT: true, // N/A
+      _ENABLE_DID_INIT_ATTRS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_ember-component-didinitattrs
+      _ENABLE_RENDER_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_rendering-into-a-render-helper-that-resolves-to-an-outlet
+      _ENABLE_REVERSED_OBSERVER_SUPPORT: true, // N/A
+      _ENABLE_INITIALIZER_ARGUMENTS_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_initializer-arity
+      _ENABLE_ROUTER_RESOURCE: true, // N/A
+      _ENABLE_CURRENT_WHEN_SUPPORT: true, // N/A
       _ENABLE_DEPRECATED_REGISTRY_SUPPORT: true,
-      _ENABLE_IMMEDIATE_OBSERVER_SUPPORT: true,
-      _ENABLE_STRING_FMT_SUPPORT: true,
-      _ENABLE_FREEZABLE_SUPPORT: true,
-      _ENABLE_COMPONENT_DEFAULTLAYOUT_SUPPORT: true,
-      _ENABLE_CONTROLLER_WRAPPED_SUPPORT: true,
+      _ENABLE_IMMEDIATE_OBSERVER_SUPPORT: true, // N/A
+      _ENABLE_STRING_FMT_SUPPORT: true, // N/A
+      _ENABLE_FREEZABLE_SUPPORT: true, // N/A
+      _ENABLE_COMPONENT_DEFAULTLAYOUT_SUPPORT: true, // https://www.emberjs.com/deprecations/v2.x/#toc_ember-component-defaultlayout
+      _ENABLE_CONTROLLER_WRAPPED_SUPPORT: true, // N/A
       _ENABLE_PROPERTY_REQUIRED_SUPPORT: true
     }
   };
