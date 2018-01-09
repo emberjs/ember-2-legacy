ec3ea48c8ffeea1c8734f5fe3fc0dc48e1a40500
Robert Jackson <rjackson@linkedin.com>
2018-01-09 15:17:36 -0500

    Refactor _actions tests to confirm expected results.

--- tests/unit/underscore-actions-test.js
+++ tests/unit/underscore-actions-test.js
@@ -2,11 +2,30 @@ import Route from '@ember/routing/route';
 import Controller from '@ember/controller';
 import Component from '@ember/component';
 import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-module('Underscore _actions');
+module('Underscore _actions', function(hooks) {
+  setupTest(hooks);
 
-test('that _actions are correctly added to Routes, Controllers, and Components', function(assert) {
-  assert.ok(Route.prototype.hasOwnProperty('_actions'), '_actions is defined on Ember.Route');
-  assert.ok(Controller.prototype.hasOwnProperty('_actions'), '_actions is defined on Ember.Controller');
-  assert.ok(Component.prototype.hasOwnProperty('_actions'), '_actions is defined on Ember.Component');
+
+
+  function confirmActionsAreHandled(type, Klass) {
+    test(`_actions defined on ${type} at extend time is properly handled`, function(assert) {
+      function definedAction() { }
+      this.owner.register(`${type}:foo`, Klass.extend({
+        // eslint-disable-next-line
+        _actions: {
+          foo: definedAction,
+        }
+      }));
+
+      let instance = this.owner.factoryFor(`${type}:foo`).create();
+
+      assert.strictEqual(instance.actions.foo, definedAction);
+    });
+  }
+
+  confirmActionsAreHandled('route', Route);
+  confirmActionsAreHandled('controller', Controller);
+  confirmActionsAreHandled('component', Component);
 });
