dd687e3417ebb4d0fe2a78ba873ac269e677361e
Travis Hoover <thoov7@gmail.com>
2018-02-14 10:26:15 -0800

    Cleaning up enumerable-contains

--- vendor/enumerable-contains.js
+++ vendor/enumerable-contains.js
@@ -10,50 +10,7 @@
     return false;
   }
 
-  const contexts = [];
-
-  function popCtx() {
-    return contexts.length === 0 ? {} : contexts.pop();
-  }
-
-  function pushCtx(ctx) {
-    contexts.push(ctx);
-    return null;
-  }
-
-  _Ember.Enumerable.reopen({
-    // This method was moved off of Enumberable with (https://github.com/emberjs/ember.js/pull/16131)
-    find(callback, target) {
-      _Ember.assert('Enumerable#find expects a function as first argument.', typeof callback === 'function');
-
-      let len = _Ember.get(this, 'length');
-
-      if (target === undefined) {
-        target = null;
-      }
-
-      let context = popCtx();
-      let found = false;
-      let last = null;
-      let next, ret;
-
-      for (let idx = 0; idx < len && !found; idx++) {
-        next = this.nextObject(idx, last, context);
-
-        found = callback.call(target, next, idx, this);
-        if (found) {
-          ret = next;
-        }
-
-        last = next;
-      }
-
-      next = last = null;
-      context = pushCtx(context);
-
-      return ret;
-    },
-
+  _Ember.Array.reopen({
     contains(obj) {
       _Ember.deprecate(
         '`Enumerable#contains` is deprecated, use `Enumerable#includes` instead.',
